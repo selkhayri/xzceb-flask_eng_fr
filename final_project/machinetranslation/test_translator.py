4daf897164a502f077d8f9b87b964fc7e99cbe97
Sami Elkhayri <selkhayri@gmail.com>
2022-10-23 10:46:13 -0400

    Corrected and renamed translator test python file

--- final_project/machinetranslation/test_translator.py
+++ final_project/machinetranslation/test_translator.py
@@ -1,21 +1,21 @@
 import unittest
 
-from translator import englishToFrench, frenchToEnglish
+from translator import english_to_french, french_to_english
 
 class TestEnglishToFrench(unittest.TestCase):
     def test1(self): 
-        self.assertEqual(englishToFrench("Hello")["translations"][0]["translation"], "Bonjour")
+        self.assertEqual(english_to_french("Hello"), "Bonjour")
 
 class TestEnglishToFrenchNull(unittest.TestCase):
     def test1(self): 
-        self.assertEqual(englishToFrench(""), "")
+        self.assertEqual(english_to_french(""), "")
 
 class TestFrenchToEnglish(unittest.TestCase):    
     def test2(self):
-        self.assertEqual(frenchToEnglish("Bonjour")["translations"][0]["translation"], "Hello")
+        self.assertEqual(french_to_english("Bonjour"), "Hello")
         
 class TestFrenchToEnglishNull(unittest.TestCase):    
     def test2(self):
-        self.assertEqual(frenchToEnglish(""), "")
+        self.assertEqual(french_to_english(""), "")
 
 unittest.main()
--- final_project/machinetranslation/translator.py
+++ final_project/machinetranslation/translator.py
@@ -24,11 +24,12 @@ language_translator.set_service_url(url)
 EN_FR = "en-fr"
 FR_EN = "fr-en"
 
-"""
-Function Docstring
-"""
+
 @app.route("/englishToFrench")
-def englishToFrench(english_text=""):
+def english_to_french(english_text=""):
+    """
+    This function translates English text to French
+    """
     if request:
         text_to_translate = request.args.get('textToTranslate')
     else:
@@ -36,18 +37,18 @@ def englishToFrench(english_text=""):
 
     if text_to_translate == "":
         french_text = ""
-    else:
-        french_text = language_translator.translate( text=text_to_translate,
+        return french_text
+
+    french_text = language_translator.translate( text=text_to_translate,
                                                  model_id=EN_FR).get_result()
+    return french_text.get("translations")[0].get("translation")
 
-    return french_text
 
-"""
-Function Docstring
-"""
 @app.route("/frenchToEnglish")
-def frenchToEnglish(french_text=""):
-
+def french_to_english(french_text=""):
+    """
+    This function translates French text to English
+    """
     if request:
         text_to_translate = request.args.get('textToTranslate')
     else:
@@ -55,20 +56,26 @@ def frenchToEnglish(french_text=""):
 
     if text_to_translate == "":
         english_text = ""
-    else:
-        english_text = language_translator.translate( text=text_to_translate,
-                                                 model_id=FR_EN).get_result()
+        return english_text
 
-    return english_text
+    english_text = language_translator.translate( text=text_to_translate,
+                                                 model_id=FR_EN).get_result()
+    return english_text.get("translations")[0].get("translation")
+
 
 @app.route("/")
-def renderIndexPage():
-    # Write the code to render template
+def render_index_page():
+    """
+    This function renders the index page
+    """
     return render_template("index.html")
+
 
 @app.route("/static/<path:path>")
 def static_dir(path):
-    print("In static_dir")
+    """
+    This function returns the file whose static path is specified in the URL
+    """
     return send_from_directory("static", path)
 
 if __name__ == "__main__":
